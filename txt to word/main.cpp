c8f348013a70dc8f8fd534d03e528e6e6b74f536
prasnakumar <prasnakumar.nayaka@gmail.com>
2020-05-16 22:56:52 +0530

    file

---
+++ txt to word/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+#include  <string.h>
+#include <ctime>
+
+using namespace std;
+char file1[10]="a.txt";
+void  insert_bike()
+{char file2[60];
+cout<<"enter the file name that you want with .txt";
+cin>>file2;
+
+
+    char id[10000];
+    fstream stud;
+    stud.open(file2,ios::app);
+    cin.ignore();
+    cout<<"enter the content of file \n";
+    cin.getline(id,1000);
+    cout<<"done";
+    stud<<id;
+    stud.close();
+}
+void changefile()
+{char filename[30],d[60];
+cout<<"enter the file name that you want to change ";
+cin>>filename;
+fstream stud;
+stud.open(filename);
+cout<<"enter the file name that you want the docx to be with extension";
+cin>>d;
+rename(filename,d);
+cout<<"renaming the file is done";
+
+}
+int main()
+{
+    int c,a;
+
+cout<<"enter 1.to open and write in a file \n 2. to covert to docx\n";
+    cin>>c;
+    switch(c){
+
+case 1:
+    insert_bike();
+    break;
+case 2:
+    changefile();
+    break;}
+
+}
